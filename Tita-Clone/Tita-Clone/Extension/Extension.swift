941257f04661334d1207b3b7d7ff94476447b319
노연주 <yjnoh1016@naver.com>
2021-12-08 12:15:30 +0900

    [UPDATE] Add Button UnderLine

--- Tita-Clone/Tita-Clone/Extension/Extension.swift
+++ Tita-Clone/Tita-Clone/Extension/Extension.swift
@@ -357,3 +357,16 @@ extension UITextField {
         self.rightViewMode = .always
     }
 }
+
+// MARK: - Button set UnderLine
+extension UIButton {
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
