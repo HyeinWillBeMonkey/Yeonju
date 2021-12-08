cc4ce5bb1dde66728ca23ed4de4e09b8dad23218
노연주 <yjnoh1016@naver.com>
2021-12-08 17:31:07 +0900

    [UPDATE] LoginButton shadow

--- Tita-Clone/Tita-Clone/Views/Buttons/LoginButton.swift
+++ Tita-Clone/Tita-Clone/Views/Buttons/LoginButton.swift
@@ -30,12 +30,11 @@ class LoginButton: UIButton {
     private func configureUI(){
         self.dynamicFont(fontSize: 18, currentFontName: "NotoSans-Bold")
         self.setTitleColor(.black, for: .normal)
-        self.clipsToBounds = true
         self.layer.cornerRadius = viewBounds.width/37.5
         self.backgroundColor = .rgb(red: 255, green: 251, blue: 235)
         
         self.layer.shadowColor = UIColor.rgba(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.layer.shadowOffset = CGSize(width: 2, height: 2)
         self.layer.shadowRadius = viewBounds.width/37.5
         self.layer.shadowOpacity = 0.3
     }
