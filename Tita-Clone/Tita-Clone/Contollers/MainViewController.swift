13715bdef85c1b693713dbf27b9033707e529efc
노연주 <yjnoh1016@naver.com>
2021-12-12 03:41:59 +0900

    [UPDATE] Add Notice\Best View

--- Tita-Clone/Tita-Clone/Contollers/MainViewController.swift
+++ Tita-Clone/Tita-Clone/Contollers/MainViewController.swift
@@ -31,22 +31,14 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
     private let titleLabel: [String] = ["오늘의 아침", "오늘의 점심", "오늘의 저녁"]
     private let menuLabel: [String] = ["백미밥\n시레기된장국\n감자채어묵볶음\n치즈계란말이\n깍두기\n시리얼/우유", "순두부찌개\n마른새우조림\n느타리버섯호박볶음\n찜닭덮밥\n깍두기\n아이스크림", "백미밥\n바지락살미역국\n영양갈비찜\n깻잎순나물\n두부쑥갓구이/양념장\n깍두기"]
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+    private let noticeView = NoticeView().then {
+        $0.dataSetting(underLineColor: .rgba(red: 255, green: 196, blue: 196, alpha: 0.75), maintext: "주요 공지글")
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = cafeteriaCollectionView.dequeueReusableCell(withReuseIdentifier: "CafeteriaCollectionViewCell", for: indexPath) as! CafeteriaCollectionViewCell
-        cell.image.image = UIImage(named: cefeteriaImage[indexPath.row])
-        cell.title.text = titleLabel[indexPath.row]
-        cell.menu.text = menuLabel[indexPath.row]
-        return cell
+    private let bestView = NoticeView().then {
+        $0.dataSetting(underLineColor: .rgba(red: 255, green: 253, blue: 196, alpha: 0.75), maintext: "BEST 게시글")
     }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize.init(width: self.view.frame.width/1.19, height: self.view.frame.height/4.34)
-    }
-    
+
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,18 +52,12 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
         view.backgroundColor = .white
         collectionViewSetting()
         addView()
-        cornerRadius()
         location()
     }
     
     // MARK: - Add View
     private func addView(){
-        [mainTopView, cafeteriaCollectionView].forEach { view.addSubview($0) }
-    }
-    
-    // MARK: - Corner Radius
-    private func cornerRadius(){
-        
+        [mainTopView, cafeteriaCollectionView, noticeView, bestView].forEach { view.addSubview($0) }
     }
     
     // MARK: - Location
@@ -89,6 +75,20 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
             make.top.equalTo(mainTopView.snp.bottom).offset(self.view.frame.height/20.82)
             make.left.equalToSuperview().offset(self.view.frame.width/12.93)
         }
+        
+        noticeView.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(1.17)
+            make.height.equalToSuperview().dividedBy(4.6)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(mainTopView.snp.bottom).offset(self.view.frame.height/3.14)
+        }
+        
+        bestView.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(1.17)
+            make.height.equalToSuperview().dividedBy(4.6)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(noticeView.snp.bottom).offset(self.view.frame.height/24.61)
+        }
     }
     
     //MARK: - collectionViewSetting
@@ -97,6 +97,21 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
         cafeteriaCollectionView.delegate = self
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = cafeteriaCollectionView.dequeueReusableCell(withReuseIdentifier: "CafeteriaCollectionViewCell", for: indexPath) as! CafeteriaCollectionViewCell
+        cell.image.image = UIImage(named: cefeteriaImage[indexPath.row])
+        cell.title.text = titleLabel[indexPath.row]
+        cell.menu.text = menuLabel[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize.init(width: self.view.frame.width/1.19, height: self.view.frame.height/4.34)
+    }
 }
 
 //MARK: - Preview
