f2c1a586ff679344d43d7f4bd7c18931a97b9326
노연주 <yjnoh1016@naver.com>
2021-12-16 20:07:42 +0900

    [UPDATE] Delete Preview

--- Tita-Clone/Tita-Clone/Contollers/NoticeViewController.swift
+++ Tita-Clone/Tita-Clone/Contollers/NoticeViewController.swift
@@ -90,30 +90,3 @@ class NoticeViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return self.view.frame.height/7.09
     }
 }
-
-//MARK: - Preview
-#if DEBUG
-import SwiftUI
-struct NoticeViewControllerRepresentable: UIViewControllerRepresentable {
-    
-func updateUIViewController(_ uiView: UIViewController,context: Context) {
-        // leave this empty
-}
-    @available(iOS 13.0.0, *)
-    func makeUIViewController(context: Context) -> UIViewController{
-        NoticeViewController()
-    }
-}
-@available(iOS 13.0, *)
-struct NoticeViewControllerRepresentable_PreviewProvider: PreviewProvider {
-    static var previews: some View {
-        Group {
-            NoticeViewControllerRepresentable()
-                .ignoresSafeArea()
-                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-        }
-        
-    }
-} #endif
-
