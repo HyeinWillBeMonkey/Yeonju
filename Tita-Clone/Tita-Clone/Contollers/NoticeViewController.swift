41243b99a57b32978ee89c24cdba0c57b438b680
노연주 <yjnoh1016@naver.com>
2021-12-16 10:52:39 +0900

    [UPDATE] Add TableView

--- Tita-Clone/Tita-Clone/Contollers/NoticeViewController.swift
+++ Tita-Clone/Tita-Clone/Contollers/NoticeViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 import SnapKit
 import Then
 
-class NoticeViewController: UIViewController {
+class NoticeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     //MARK: - Properties
     private let topView = TopView().then {
         $0.dataSetting(noticeNameString: "자유게시판")
+    }
+    
+    private let noticeTableView = UITableView().then {
+        $0.showsVerticalScrollIndicator = false
+        $0.separatorStyle = .singleLine
     }
     
     //MARK: - Lifecycle
@@ -29,11 +35,12 @@ class NoticeViewController: UIViewController {
         addView()
         cornerRadius()
         location()
+        tableViewSetting()
     }
     
     // MARK: - Add View
     private func addView(){
-        [topView].forEach{ view.addSubview($0) }
+        [topView, noticeTableView].forEach{ view.addSubview($0) }
     }
     
     // MARK: - Corner Radius
@@ -49,8 +56,39 @@ class NoticeViewController: UIViewController {
             make.width.equalToSuperview()
             make.height.equalToSuperview().dividedBy(7.59)
         }
+        
+        noticeTableView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(topView.snp.bottom).offset(self.view.frame.height/38.10)
+            make.width.equalToSuperview().dividedBy(1.32)
+            make.height.equalToSuperview()
+        }
     }
     
+    private func tableViewSetting(){
+        noticeTableView.dataSource = self
+        noticeTableView.delegate = self
+        
+        noticeTableView.register(FreeNoticeTableViewCell.self, forCellReuseIdentifier: FreeNoticeTableViewCell.FreeNoticeTableViewCellIdentifier)
+        
+        noticeTableView.separatorInset.left = 0
+        noticeTableView.separatorInset.right = 0
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = noticeTableView.dequeueReusableCell(withIdentifier: "FreeNoticeTableViewCell") as! FreeNoticeTableViewCell
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.view.frame.height/7.09
+    }
 }
 
 //MARK: - Preview
