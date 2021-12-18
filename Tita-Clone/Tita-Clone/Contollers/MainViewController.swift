ee39e2402c1cc7f2f680fc851491a03d4512a89d
노연주 <yjnoh1016@naver.com>
2021-12-18 16:40:17 +0900

    [UPDATE] Use DataSetting

--- Tita-Clone/Tita-Clone/Contollers/MainViewController.swift
+++ Tita-Clone/Tita-Clone/Contollers/MainViewController.swift
@@ -12,6 +12,11 @@ import Then
 class MainViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     //MARK: - Properties
+    
+    private let cefeteriaImage: [String] = ["Tita-Breakfast", "Tita-Lunch", "Tita-Dinner"]
+    private let titleLabel: [String] = ["오늘의 아침", "오늘의 점심", "오늘의 저녁"]
+    private let menuLabel: [String] = ["백미밥\n시레기된장국\n감자채어묵볶음\n치즈계란말이\n깍두기\n시리얼/우유", "순두부찌개\n마른새우조림\n느타리버섯호박볶음\n찜닭덮밥\n깍두기\n아이스크림", "백미밥\n바지락살미역국\n영양갈비찜\n깻잎순나물\n두부쑥갓구이/양념장\n깍두기"]
+    
     private let mainTopView = MainTopBarView().then {
         $0.dataSetting(schoolName: "광주소프트웨어마이스터고")
     }
@@ -26,10 +31,6 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
         $0.backgroundColor = .white
         $0.collectionViewLayout = layout
     }
-    
-    private let cefeteriaImage: [String] = ["Tita-Breakfast", "Tita-Lunch", "Tita-Dinner"]
-    private let titleLabel: [String] = ["오늘의 아침", "오늘의 점심", "오늘의 저녁"]
-    private let menuLabel: [String] = ["백미밥\n시레기된장국\n감자채어묵볶음\n치즈계란말이\n깍두기\n시리얼/우유", "순두부찌개\n마른새우조림\n느타리버섯호박볶음\n찜닭덮밥\n깍두기\n아이스크림", "백미밥\n바지락살미역국\n영양갈비찜\n깻잎순나물\n두부쑥갓구이/양념장\n깍두기"]
     
     private let noticeView = NoticeView().then {
         $0.dataSetting(underLineColor: .rgba(red: 255, green: 196, blue: 196, alpha: 0.75), maintext: "주요 공지글")
@@ -103,9 +104,7 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = cafeteriaCollectionView.dequeueReusableCell(withReuseIdentifier: "CafeteriaCollectionViewCell", for: indexPath) as! CafeteriaCollectionViewCell
-        cell.image.image = UIImage(named: cefeteriaImage[indexPath.row])
-        cell.title.text = titleLabel[indexPath.row]
-        cell.menu.text = menuLabel[indexPath.row]
+        cell.dataSetting(imageName: cefeteriaImage[indexPath.row], titleLabel: titleLabel[indexPath.row], menuLabel: menuLabel[indexPath.row])
         return cell
     }
     
@@ -113,29 +112,3 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
         return CGSize.init(width: self.view.frame.width/1.19, height: self.view.frame.height/4.34)
     }
 }
-
-//MARK: - Preview
-#if DEBUG
-import SwiftUI
-struct MainViewControllerRepresentable: UIViewControllerRepresentable {
-    
-func updateUIViewController(_ uiView: UIViewController,context: Context) {
-        // leave this empty
-}
-    @available(iOS 13.0.0, *)
-    func makeUIViewController(context: Context) -> UIViewController{
-        MainViewController()
-    }
-}
-@available(iOS 13.0, *)
-struct MainViewControllerRepresentable_PreviewProvider: PreviewProvider {
-    static var previews: some View {
-        Group {
-            MainViewControllerRepresentable()
-                .ignoresSafeArea()
-                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-        }
-        
-    }
-} #endif
